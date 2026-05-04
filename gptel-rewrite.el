aagit/gptel

commit 992161fd06eb2435214427b1d3d9d7ed3c46d4f2

gptel-rewrite: Confirm tool calls from minibuffer
* gptel-rewrite.el (gptel--rewrite-callback): Confirm tool calls
from the minibuffer, not a buffer overlay.  This behavior is
certainly more annoying since it steals focus via `read-key'.  But
there are two advantages that win out:

1. When the region being rewritten is tall, the in-buffer overlay
ends up below the window bounds and out of sight, leading to
confusion about the status of the rewrite.

2. The default tool call previewer inserts (preview) text into the
buffer.  This is fine in the context of chat buffers, but can have
unintended side effects in buffers with linters or LSP active.

(gptel--suffix-rewrite): Don't set the `category' overlay property
of the rewrite overlay.  This is meant to be used for inheriting
overlay properties from symbol properties, which we don't mean to
do here.

Add some outline headings.

gptel-rewrite.el
@@ -192,6 +192,8 @@ CALLBACK is supplied by Eldoc, see
                (format (substitute-command-keys "%s rewrite available: accept \\[gptel--rewrite-accept], iterate \\[gptel--rewrite-iterate], clear \\[gptel--rewrite-reject], merge \\[gptel--rewrite-merge], diff \\[gptel--rewrite-diff] or ediff \\[gptel--rewrite-ediff]")
                        (propertize (gptel--model-name gptel-model) 'face 'mode-line-emphasis)))))
 
+;; ** Navigation across rewrite regions
+
 (defun gptel--rewrite-move (search-func)
   "Move directionally to a gptel rewrite location using SEARCH-FUNC."
   (let* ((ov (cdr (get-char-property-and-overlay (point) 'gptel-rewrite)))
@@ -216,6 +218,8 @@ CALLBACK is supplied by Eldoc, see
   "Go to previous pending LLM rewrite in buffer, if one exists."
   (interactive)
   (gptel--rewrite-move #'previous-single-char-property-change))
+
+;; ** Rewrite actions helpers
 
 (defun gptel--rewrite-overlay-at (&optional pt)
   "Check for a gptel rewrite overlay at PT and return it.
@@ -508,7 +512,7 @@ INFO is the async communication channel for the rewrite request."
           (insert response)
           (unless (eobp) (ignore-errors (delete-char (length response))))
           (font-lock-ensure)
-          (overlay-put ov 'display (buffer-string))))
+          (overlay-put ov 'display (propertize (buffer-string) 'face 'default))))
       (unless (plist-get info :stream) (gptel--rewrite-callback t info)))
 
      ((eq response 'abort)              ;request aborted
@@ -517,8 +521,10 @@ INFO is the async communication channel for the rewrite request."
       (delete-overlay ov))
 
      ((eq (car-safe response) 'tool-call) ;tool call confirmation
-      (gptel--display-tool-calls          ;use minibuffer if buffer is read-only
-       (cdr response) info (buffer-local-value 'buffer-read-only buf)))
+      (gptel--rewrite-update-status ov " Run tools?" '(mode-line-emphasis default))
+      (gptel--display-tool-calls   ;use minibuffer
+       (cdr response) info         ;; (buffer-local-value 'buffer-read-only buf)
+       t))
 
      ((null response)                   ;finished with error
       (message (concat "LLM response error: %s. Rewrite in buffer %s canceled.")
@@ -737,7 +743,6 @@ generated from functions."
              :context
              (let ((ov (or (cdr-safe (get-char-property-and-overlay (point) 'gptel-rewrite))
                            (make-overlay (region-beginning) (region-end) nil t))))
-               (overlay-put ov 'category 'gptel)
                (overlay-put ov 'evaporate t)
                ;; NOTE: Switch to `generate-new-buffer' after we drop Emacs 27.1 (#724)
                (cons ov (gptel--temp-buffer " *gptel-rewrite*")))

commit 5cf1f6f7772438e29cfbd4d19b1d5893079e346f

gptel-rewrite: remove default face from overlay display
A previous commit added the 'default' face to the overlay's display
property. This causes the pre-rewrite text to look identical to the
post-rewrite text, which prevents to see where the cursor is
positioned during inference streaming.

The keyboard can't scroll and that can't be fixed in gptel, but at
least the mouse wheel can scroll with scroll-preserve-screen-position
so it'd be nice to retain the visibility of the inference progress
while the text doesn't change.

Fixes: 992161fd06eb ("gptel-rewrite: Confirm tool calls from minibuffer")

gptel-rewrite.el
@@ -572,7 +572,7 @@ INFO is the async communication channel for the rewrite request."
           (insert response)
           (unless (eobp) (ignore-errors (delete-char (length response))))
           (font-lock-ensure)
-          (overlay-put ov 'display (propertize (buffer-string) 'face 'default))))
+          (overlay-put ov 'display (propertize (buffer-string)))))
       (unless (plist-get info :stream) (gptel--rewrite-callback t info)))
 
      ((eq response 'abort)              ;request aborted